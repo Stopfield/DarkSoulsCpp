e6eac7b9d9cc77614222815bd550eb75f3845215
Stopfield <thiagoparacampo@hotmail.com>
2023-09-25 23:34:09 -0300

    Demonstração atividade 2

--- main.cpp
+++ main.cpp
@@ -1,48 +1,50 @@
 #include <iostream>
 
 #include "weapon.h"
+#include "player.h"
 
 int main()
 {
+    /* Player não constante - arma constante */
+    Player player("Thiago");
+
     // Criando uma espada com 253.3 de dano e durabilidade de 150
     Weapon espada("Astora Sword", 252.3, 150);
+    const Weapon draconian("Draconian Sword", 150.0, 75);
 
-    espada.printStatus();
+    player.equipWeapon(espada);
 
-    // Atacou um inimigo por exemplo
-    cout << "Ataca um inimigo com " << espada.getName() << "\n";
-    cout << endl;
+    const Weapon * varWeapon = &player.getEquipedWeapon();
+    std::cout << "Var: " << varWeapon->getName() << std::endl;
 
-    cout << "Durabilidade antes do ataque: " << espada.getDurability() << "\n";
-    cout << "Vida do Inimigo: 260.0 - " << espada.getDamage() << " = " << espada.inflictDamage(260.0) << "\n";
-    cout << "Durabilidade atual da espada: " << espada.getDurability() << "\n";
-    cout << endl;
+    // Pode atacar, não é constante
+    player.attack();
 
-    cout << "Personagem afiou a espada e dobrou seu ataque!\n";
-    espada.setDamage( espada.getDamage() * 2 );
+    std::cout << player.getEquipedWeapon().getDurability() << std::endl;
 
-    cout << "Dano atual da espada: " << espada.getDamage() << "\n";
-    cout << "Ataca um inimigo com: " << espada.getName() << "\n";
-    cout << "Vida do inimigo: 260.0 - "
-        << espada.getDamage()
-        << " = "
-        << espada.inflictDamage(260.0) << "\n";
+    /* E com uma constante? */
+    varWeapon = &draconian;
 
-    cout << endl;
+    std::cout << "Var: " << varWeapon->getName() << std::endl;
+    std::cout << "Player: " << player.getEquipedWeapon().getName() << std::endl;
 
-    // O personagem usou muito a espada e ela quebrou!
-    cout << "O personagem usou muito a espada e ela quebrou!\n";
-    espada.setDurability(0);
+    /* E com const? Nem pode! O atributo deveria ser const também! */
+    // player.equipWeapon(draconian);
 
-    cout << endl;
+    const Player constancio("Constâncio");
 
-    cout << "Durabilidade da espada: " << espada.getDurability() << "\n";
-    espada.inflictDamage(100);
+    // A arma não é constante, então pode
+    constancio.attack();
 
-    cout << endl;
+    // Sempre verificar se está sem equipamento
+    if (!constancio.isUnarmed())
+    {
+        std::cout << "Constancio's Weapon: " <<
+                    constancio.getEquipedWeapon().getDamage() << "\n";
+    }
 
-    Weapon espadaVazia;
-    espadaVazia.printStatus();
+    /* Não pode! */
+    // constancio.equipWeapon(espada);
 
     return 0;
 }
